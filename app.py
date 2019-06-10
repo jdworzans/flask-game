ecc84a4b87615c136bdcd0610c6a1c6489175d03
Jakub Dworzański <249703@student.pwr.edu.pl>
2019-06-10 18:04:43 +0200

    added extensions to base.html

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+
 @app.route("/")
-def index():
+def play():
+    return(render_template("game.html"))
+
+@app.route("/help")
+def help():
+    return(render_template("instruction.html"))
+
+@app.route("/about")
+def about():
     return(render_template("base.html"))
