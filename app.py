32da794e88f24d82d2be3f26396a8c3190091e35
Jakub Dworzański <249703@student.pwr.edu.pl>
2019-06-11 11:38:20 +0200

    some changes to repair website

--- app.py
+++ app.py
@@ -4,22 +4,14 @@ app = Flask(__name__)
 
 
 
-@app.route("/")
-<<<<<<< HEAD
-def index():
-    return(render_template("base.html"))
-
-@app.route("/play")
-def play():
-    return(render_template("jschessboard.html"))
-=======
-
-
 @app.route("/help")
 def help():
     return(render_template("instruction.html"))
 
+@app.route("/")
+def play():
+    return(render_template("game.html"))
+
 @app.route("/about")
 def about():
-    return(render_template("base.html"))
->>>>>>> a629b9973f1beaaf9732d63c8a726bbc6055c269
+    return(render_template("about.html"))
