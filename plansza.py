50e2bca62e83b7749ca04bd83885d3e65bb0f09f
Jakub Dworzański <249703@student.pwr.edu.pl>
2019-06-16 17:47:48 +0200

    labels and new grid added

--- plansza.py
+++ plansza.py
@@ -11,15 +11,18 @@ def rysuj(lista, col1, col2):
     nowa = odejmowanie(lista)
     iksy, igreki = [], []
     for k in nowa:
-        iksy.append(k[0])
-        igreki.append(k[1])
+        igreki.append(k[0])
+        iksy.append(k[1])
     fig, axis = plt.subplots(1,1)
     axis.scatter(iksy[0::2], igreki[0::2], s = 7000, c=col1)
     axis.scatter(iksy[1::2], igreki[1::2], s = 7000, c=col2)
     axis.set_xlim(0,3)
     axis.set_ylim(0,3)
-    axis.set_xticks([1,2,3])
-    axis.set_yticks([1,2,3])
+    axis.vlines([1,2],0,3)
+    axis.hlines([1,2],0,3)
+    axis.set_xticks([0.5, 1.5, 2.5])
+    axis.set_xticklabels(["A", "B", "C"])
+    axis.set_yticks([0.5, 1.5, 2.5])
+    axis.set_yticklabels(["1", "2", "3"])
     axis.tick_params(left = False, bottom = False)
-    axis.grid()
     return fig
