d54192c34fd502c3c9c5f00aab44af25f16166bb
Emilia Kowal <249716@student.pwr.edu.pl>
2019-06-19 12:17:24 +0200

    docstring ciąg dalszy

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.secret_key = b'technologie informacyjne'
 
 @app.route('/', methods=["GET", "POST"])
 def start():
-    """Returns a start page"""
+    """Returns a start page."""
     form = startForm()
     if request.method == "POST":
         col1 = form.colour1.raw_data
@@ -34,7 +34,7 @@ def start():
 @app.route("/play", methods=["GET", "POST"])
 def play():
     """Returns win or draw or game template depending on 
-    session status"""
+    session status."""
     if session["win"]:
         return(render_template("win.html", context=session, str=str))
     if len(session['progress']) == 9:
@@ -43,9 +43,13 @@ def play():
 
 @app.route("/play/<field>/")
 def move(field):
-    """Returns status of game
+    """Returns status of game.
     
-    Args: field (str):  """
+    Args: 
+        field (str): contains movement coords.
+    
+    Returns: 
+        redirects session data to play function.  """
     winnings = [set([(1,1), (1,2), (1,3)]),
                 set([(2,1), (2,2), (2,3)]),
                 set([(3,1), (3,2), (3,3)]),
@@ -73,14 +77,17 @@ def move(field):
 
 @app.route("/help")
 def help():
+    """ Renders instruction template."""
     return(render_template("instruction.html"))
 
 @app.route("/about")
 def about():
+    """Renders about template."""
     return(render_template("about.html"))
 
 @app.route('/plot.png')
 def plot_png():
+    """Returns a gameboard in png format"""
     fig = draw(session['progress'], session['col1'], session['col2'])
     output = io.BytesIO()
     FigureCanvasAgg(fig).print_png(output)
