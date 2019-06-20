63c5f3dd29e726ff243e6d336626587399f96fcc
Jakub Dworzański <249703@student.pwr.edu.pl>
2019-06-20 12:24:34 +0200

    accessibility changes

--- forms.py
+++ forms.py
@@ -5,12 +5,12 @@ from wtforms_components import ColorField
 
 class gameForm(FlaskForm):
     row = RadioField(choices=[(1, "1"), (2, "2"), (3, "3")], default=1)
-    col = RadioField(choices=[(1, 'A'), (2, 'B'), (3, 'C')], default=1)
+    col = RadioField(choices=[(1, "A"), (2, "B"), (3, "C")], default=1)
 
 class startForm(FlaskForm):
-    colour1 = ColorField(label="colour1", default="#ff0000")
-    colour2 = ColorField(label="colour2", default="#000000")
-    username1 = StringField('Username 1:', default="Gracz 1")
-    username2 = StringField('Username 2:', default="Gracz 2")
+    colour1 = ColorField("Kolor pierwszego gracza", default="#ff0000")
+    colour2 = ColorField("Kolor drugiego gracza", default="#000000")
+    username1 = StringField("Nazwa pierwszego gracza", default="Gracz 1")
+    username2 = StringField("Nazwa drugiego gracza", default="Gracz 2")
 
 
